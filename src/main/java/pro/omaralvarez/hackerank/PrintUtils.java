205686ca4f2650a7b52d6e0644772260b23856f0
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2024-03-30 13:17:47 -0600

    It adds a utility to print and array

---
+++ src/main/java/pro/omaralvarez/hackerank/PrintUtils.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pro.omaralvarez.hackerank;
+
+/**
+ *
+ * @author ignis
+ */
+public class PrintUtils {
+
+    public static void printArray(String[] array) {
+        for (String element : array) {
+            System.out.print(element);
+        }
+        System.out.println("");
+    }
+
+    public static void printArray(int[] array) {
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println("");
+    }
+
+}
