57a69535a72ca02c62de5fae3343fd805ecfe4d5
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2024-06-01 23:36:18 -0600

    formats

--- src/main/java/pro/omaralvarez/hackerank/WordNumber.java
+++ src/main/java/pro/omaralvarez/hackerank/WordNumber.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 public class WordNumber {
     
     public static void main(String[] args) throws Exception {
+        
         var numbers = "onesixeightfournine";
         var result = wordToNumber(numbers);
         System.out.println(result);
